a894c7fad423d2c6affaf4f38f0aeb35a4f1aee4
Knut Eriksen <knutfe@uia.no>
2025-01-28 02:07:21 +0100

    Added yfinance, added start and end date, fixed column names for prophet

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import pandas as pd
 from prophet import Prophet
+import yfinance as yf
+from datetime import datetime, timedelta
 
-#Read csv
-df = pd.read_csv('sp500_index.csv')
+#Add live time as end date and 10 years before as start date
+end_date = datetime.today()
+start_date = end_date - timedelta(days = 10*365)
 
-#Check csv content
-print(df)
-print(df.info())
+#Test using QQQ ticker with start and end parameters
+data = yf.download("QQQ", start_date, end_date)
+
+#Reset index of dataframe
+data.reset_index(inplace=True)
+
+#Create dataframe
+close_df = pd.DataFrame()
+
+#Edit column names to use prophet
+close_df["ds"] = data["Date"]
+close_df["y"] = data["Close"]
+
+print(close_df)
 
 #Initiate Prophet and start fit
 m = Prophet()
-m.fit(df)
-
+m.fit(close_df)
